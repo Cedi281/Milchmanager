a4ad7e8ed16cba5f6f32825f606e1c6d5efcd605
Cedi281 <stiefvatercedric@gmail.com>
2025-07-16 20:42:04 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('milchtank').then(cache => cache.addAll([ './', './index.html', './manifest.json', './icon.png' ]))
-  );
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
